7775e5434057e95861b4263e0f7d33b841eec6a8
Paul Senior <paul.senior@cqc.org.uk>
2019-03-11 13:22:23 +0000

    Added 'using System' to startup.cs

--- SYE/Startup.cs
+++ SYE/Startup.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using GDSHelpers;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
