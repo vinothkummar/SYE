e906b14e5b86bbcd9959adbd82be62e1eadb0a02
Martin Bentley <BentleyM2@cqc.org.uk>
2019-04-16 10:58:45 +0100

    Completed POC version 4

---
+++ SYE/Helpers/SubmissionHelper.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace SYE.Helpers
+{
+    public static class SubmissionHelper
+    {
+        /// <summary>
+        /// converts a list of strings to a list of SelectListItems
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static List<SelectItem> ConvertList(IEnumerable<string> source)
+        {
+            var returnList = source?.Select(x => new SelectItem {Text = x, Selected = false}).ToList();
+
+            return returnList;
+        }
+        /// <summary>
+        /// Generated a reference number based on 2 strings
+        /// if any of the strings are null or empty then the reference number is empty
+        /// </summary>
+        /// <param name="string1"></param>
+        /// <param name="string2"></param>
+        /// <param name="numChars"></param>
+        /// <returns></returns>
+        public static string GenerateUserRef(string string1, string string2, int numChars)
+        {
+            var s1 = string.Empty;
+            var s2 = string.Empty;
+            var separator = "-";
+
+            if (string.IsNullOrWhiteSpace(string1))
+            {
+                return string.Empty;
+            }
+            else
+            {
+                if (string1.Length <= numChars)
+                {
+                    s1 = string1;
+                }
+                else
+                {
+                    s1 = string1.Substring(string1.Length - (numChars), numChars);
+                }
+            }
+
+            if (string.IsNullOrWhiteSpace(string2))
+            {
+                return string.Empty;
+            }
+            else
+            {
+                if (!string.IsNullOrWhiteSpace(string2))
+                {
+                    if (string2.Length <= numChars)
+                    {
+                        s2 = string2;
+                    }
+                    else
+                    {
+                        s2 = string2.Substring(string2.Length - (numChars), numChars);
+                    }
+                }
+
+            }
+
+            var returnString = s1 + separator + s2;
+
+            return returnString.ToUpper();
+        }
+
+    }
+
+}
