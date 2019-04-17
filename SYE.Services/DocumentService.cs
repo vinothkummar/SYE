1687e0758071c63db487a62a68fca4fa937b0510
Martin Bentley <BentleyM2@cqc.org.uk>
2019-04-17 09:29:13 +0100

    Built Document service

---
+++ SYE.Services/DocumentService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SYE.Services
+{
+    public interface IDocumentService
+    {
+        Task<bool> CreateDocumentFromJson(string json);
+    }
+    public class DocumentService : IDocumentService
+    {
+        public Task<bool> CreateDocumentFromJson(string json)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SYE.Tests/Services/DocumentServiceTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using FluentAssertions;
+using Xunit;
+
+namespace SYE.Tests.Services
+{
+    public class DocumentServiceTests
+    {
+        private string _dir = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName + "\\Resources\\";
+        private string _fileName = "submission-schema.json";
+        private string _location = "Test Location";
+
+        [Fact]
+        public void CreateDocumentTest()
+        {
+            var json = GetJsonString();
+
+            json.Should().Be("");
+        }
+
+        /// <summary>
+        /// this method reads a json file from the folder and returns the next page
+        /// </summary>
+        /// <param name="pageId"></param>
+        /// <param name="path"></param>
+        /// <param name="locationName"></param>
+        /// <returns></returns>
+        /// <remarks>
+        /// Please refactor this function (and all tests consuming this method) so method accepts whole form schema and returns required page.
+        /// If we need to load form from database/cache/session/file-system it has to be done as a seperate function
+        /// </remarks>   
+        private string GetJsonString()
+        {
+            var file = string.Empty;
+            var path = _dir + _fileName;
+            if (string.IsNullOrWhiteSpace(path))
+            {
+                throw new ArgumentException(nameof(path));
+            }
+
+            using (var r = new StreamReader(path))
+            {
+                file = r.ReadToEnd();
+            }
+
+            return file;
+        }
+
+    }
+}
