39aeaf746095059ba589c227cb3ada83cb76f291
Paul Senior <paul.senior@cqc.org.uk>
2019-05-16 16:27:57 +0100

    Updated site and schema to make the back link point to the correct page

---
+++ SYE/ViewModels/AppSettingsVM.cs
@@ -0,0 +1,11 @@
+﻿
+namespace SYE.ViewModels
+{
+    public class ApplicationSettings
+    {
+        public string AppName { get; set; }
+        public string FormStartPage { get; set; }
+        public string ServiceNotFoundPage { get; set; }
+        public string DefaultBackLink { get; set; }
+    }
+}
