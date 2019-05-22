cf9288f59a7c8b973377eefda959ae8d8a2fabc2
Prashant Thakar <ThakarP@cqc.org.uk>
2019-05-22 12:59:11 +0100

    Updated Application Settings for Get help Section.

--- SYE/ViewModels/ApplicationSettings.cs
+++ SYE/ViewModels/ApplicationSettings.cs
@@ -1,11 +1,19 @@
 ﻿
 namespace SYE.ViewModels
 {
+    public class GetHelp
+    {
+        public string ContactNumber { get; set; }
+        public string ContactHours { get; set; }
+        public string ContactExcluding { get; set; }
+    }
+
     public class ApplicationSettings
     {
         public string AppName { get; set; }
         public string FormStartPage { get; set; }
         public string ServiceNotFoundPage { get; set; }
         public string DefaultBackLink { get; set; }
+        public GetHelp GetHelp { get; set; }
     }
 }
