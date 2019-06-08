4f5d1cc5d36df3286ad49fb5ec9fdf8efe83e43f
Paul Senior <paul.senior@cqc.org.uk>
2019-06-08 17:57:11 +0100

    Added Cookie bar, fixed styling issues

--- SYE/wwwroot/js/site.js
+++ SYE/wwwroot/js/site.js
@@ -26,3 +26,39 @@ function showWhen($this, answer, valToCheck) {
         $this.hide();
     }
 }
+
+
+(function () {
+
+    var cookieSeen = getCookie("gfc-cookie-info-seen");
+    if (cookieSeen !== 'true') {
+        jQuery("#global-cookie-message").show();
+    }
+
+    jQuery("#cookie-dismiss-button").on("click", function () {
+        setCookie("gfc-cookie-info-seen", "true", 365);
+        jQuery("#global-cookie-message").hide();
+    });
+
+})();
+
+
+function setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
