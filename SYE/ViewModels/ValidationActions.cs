d30f63bd04bcbd70f134dbe8ac592f2eb61ab299
Martin Bentley <BentleyM2@cqc.org.uk>
2019-08-09 11:44:58 +0100

    Initial validation changes

---
+++ SYE/ViewModels/ValidationActions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SYE.ViewModels
+{
+    public class ValidationActions
+    {
+        public bool IsError { get; set; }
+        public string ErrorMessage { get; set; }
+        public bool ShowIncompletedSearchMessage { get; set; }
+        public bool ShowExceededMaxLengthMessage { get; set; }
+    }
+}
