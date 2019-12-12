8c681cde3077cbc82bfbc73999cf6dfa20f6f48e
seniorp <paul.senior@cqc.org.uk>
2019-12-12 10:50:31 +0000

    Moved all Inline css to site.css

--- SYE/Startup.cs
+++ SYE/Startup.cs
@@ -11,6 +11,8 @@ using SYE.MiddlewareExtensions;
 using SYE.Models;
 using System;
 using Microsoft.AspNetCore.CookiePolicy;
+using Microsoft.AspNetCore.Rewrite;
+using Microsoft.AspNetCore.StaticFiles;
 
 namespace SYE
 {
@@ -20,7 +22,7 @@ namespace SYE
 
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;      
+            Configuration = configuration;
         }
 
         public void ConfigureServices(IServiceCollection services)
@@ -31,29 +33,37 @@ namespace SYE
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
-            services.ConfigureApplicationCookie(options =>
+            services.Configure<MvcOptions>(options =>
             {
-               options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+                options.Filters.Add(new RequireHttpsAttribute());
             });
 
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+            });
+
+            services.Configure<CookieTempDataProviderOptions>(options =>
+                options.Cookie.Name = "GFC-Temp-Data-Cookie"
+            );
 
             services.AddDistributedMemoryCache();
 
             services.AddSession(options =>
             {
                 options.Cookie.HttpOnly = true;
+                options.Cookie.Name = "GFC-Session-Cookie";
                 options.IdleTimeout = TimeSpan.FromMinutes(120);
                 options.Cookie.IsEssential = true;
-                options.Cookie.SameSite = SameSiteMode.Strict;
+                options.Cookie.SameSite = SameSiteMode.Lax;
                 options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
             });
 
-            
-
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
             services.AddHttpContextAccessor();
-            services.AddOptions();            
-            services.AddCustomServices(Configuration);            
+            services.AddOptions();
+            services.AddCustomServices(Configuration);
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
@@ -61,8 +71,28 @@ namespace SYE
             app.Use(async (context, next) =>
             {
                 context.Response.Headers.Add("X-Xss-Protection", "1");
+                context.Response.Headers.Add("X-Frame-Options", "SAMEORIGIN");
+                context.Response.Headers.Add("X-Content-Type-Options", "nosniff");
+
                 await next();
             });
+
+            app.UseCsp(opts => opts
+                .BlockAllMixedContent()
+                .StyleSources(s => s.Self())
+                .StyleSources(s => s.Self())
+                .FontSources(s => s.Self())
+                .FormActions(s => s.Self())
+                .FrameAncestors(s => s.Self())
+                .ImageSources(s => s.Self())
+                .ScriptSources(s => s.UnsafeInline().Self())
+            );
+
+            app.UseHsts(h => h.MaxAge(365).IncludeSubdomains().Preload());
+            app.UseReferrerPolicy(opts => opts.NoReferrer());
+            app.UseXXssProtection(options => options.EnabledWithBlockMode());
+            app.UseXfo(options => options.SameOrigin());
+            app.UseXContentTypeOptions();
 
             if (env.IsDevelopment() || env.IsLocal())
             {
@@ -76,7 +106,7 @@ namespace SYE
                 app.UseExceptionHandler("/Error/500");
                 app.UseStatusCodePagesWithReExecute("/Error/{0}");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                //app.UseHsts();
             }
 
             app.UseHttpsRedirection();
@@ -87,10 +117,19 @@ namespace SYE
             {
                 HttpOnly = HttpOnlyPolicy.Always,
                 Secure = CookieSecurePolicy.Always,
-                MinimumSameSitePolicy = SameSiteMode.Strict
+                MinimumSameSitePolicy = SameSiteMode.Lax
             });
 
-            app.UseSession();           
+            app.UseSession();
+
+            app.UseRewriter(
+                new RewriteOptions()
+                    .AddRedirectToHttps(StatusCodes.Status301MovedPermanently, 63423)
+                );
+
+
+            var provider = new FileExtensionContentTypeProvider { Mappings = { [".webmanifest"] = "application/manifest+json" } };
+            app.UseStaticFiles(new StaticFileOptions { ContentTypeProvider = provider });
 
             app.UseMvc(routes =>
             {
